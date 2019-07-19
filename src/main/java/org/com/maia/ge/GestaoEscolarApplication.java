0af9c7832822595dc3d8a9276aff19d9215ef1c8
Dowglas Maia <dowglasmaia@gmail.com>
2019-07-19 14:55:01 -0300

    Start Project

---
+++ src/main/java/org/com/maia/ge/GestaoEscolarApplication.java
@@ -0,0 +1,13 @@
+package org.com.maia.ge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoEscolarApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/com/maia/ge/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.com.maia.ge;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GestaoEscolarApplication.class);
+	}
+
+}
