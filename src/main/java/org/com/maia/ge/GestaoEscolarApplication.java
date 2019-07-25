aa50a7ae3812047523912ea9455015ac819feab2
Dowglas Maia <dowglasmaia@gmail.com>
2019-07-25 16:39:12 -0300

    Setting up MySQL database connection

--- src/main/java/org/com/maia/ge/GestaoEscolarApplication.java
+++ src/main/java/org/com/maia/ge/GestaoEscolarApplication.java
@@ -1,13 +1,23 @@
 package org.com.maia.ge;
 
+import java.util.TimeZone;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GestaoEscolarApplication {
+public class GestaoEscolarApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoEscolarApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		// Definindo o Time-Zone Local - Resolvendo problema com horas no banco de Dados
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 	}
 
 }
---
+++ src/main/java/org/com/maia/ge/service/util/DBTestService.java
@@ -0,0 +1,9 @@
+package org.com.maia.ge.service.util;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DBTestService {
+
+	
+}
